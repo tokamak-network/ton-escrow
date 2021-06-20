c4672f1ed0cbc0a4b277903a61467399d8498882
HyukSang <zzooppii@gmail.com>
2021-06-20 23:51:15 +0900

    Deploy && verify
    deploy by rinkeby server
    address : 0xCEA6e5F2d46EaD8FA5E037b98bb6Bd1C766b9eC3
    
    and verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,20 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv/config");
+require("@nomiclabs/hardhat-etherscan");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [ `${process.env.RINKEBY_PRIVATE_KEY}` ]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
---
+++ scripts/deploy.ts
@@ -0,0 +1,25 @@
+const { ethers } = require('hardhat')
+
+async function main() {
+    const [deployer] = await ethers.getSigners()
+    const tonAddress = "0x44d4F5d89E9296337b8c48a332B3b2fb2C190CD0"
+    const rinkManagerAddress = "0x6DBce262B7b32806B823c73dB9044dDAf2D63275"
+
+    console.log("Deploying contract with the account :", deployer.address)
+
+    const tonEscrow = await ethers.getContractFactory('TONEscrow')
+    const escrow = await tonEscrow.deploy(tonAddress)
+
+    console.log("tonEscrow Address: ", escrow.address)
+    await escrow.deployed()
+    // await escrow.connect(deployer).transferOwnership(manager.address)
+    await escrow.connect(deployer).transferOwnership(rinkManagerAddress)
+    const escrowTonAddress = await escrow.ton()
+    console.log("escrowTonAddress : ", escrowTonAddress)
+}
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
